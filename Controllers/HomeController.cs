c03b89e36f260a8e4e1e90edf39791f89a2b6141
Warley Afonso <warleysilva365@gmail.com>
2023-05-28 18:41:42 -0300

    Melhorando a API adicionando o 'IActionResult' e adicionando os status code de erro

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,40 +8,43 @@ namespace Todo_API_ASPNET.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public List<TodoModel> Get([FromServices] AppDbContext context)
+        public IActionResult Get([FromServices] AppDbContext context)
         {
-            return context.Todos.ToList();
+            return Ok(context.Todos.ToList());
         }
 
         [HttpPost("/")]
-        public TodoModel Post(
+        public IActionResult Post(
                 [FromBody] TodoModel todo,
                 [FromServices] AppDbContext context)
         {
 
             context.Todos.Add(todo);
             context.SaveChanges();
-            return todo;
+            return Created($"/{todo.Id}", todo);
         }
         // Buscando por uma id
         [HttpGet("/{id:int}")]
-        public TodoModel GetById(
+        public IActionResult GetById(
                 [FromRoute] int id,
                 [FromServices] AppDbContext context)
         {
-            return context.Todos.FirstOrDefault(x => x.Id == id);
+            var todos = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (todos == null)
+                return NotFound();
+            return Ok(todos);
         }
 
         // Atualizando o registro
         [HttpPut("/{id:int}")]
-        public TodoModel Put(
+        public IActionResult Put(
                 [FromRoute] int id,
                 [FromBody] TodoModel todo,
                 [FromServices] AppDbContext context)
         {
             var model = context.Todos.FirstOrDefault(x => x.Id == id);
             if (model == null)
-                return todo;
+                return NotFound();
 
             model.Title = todo.Title;
             model.Done = todo.Done;
@@ -49,20 +52,22 @@ namespace Todo_API_ASPNET.Controllers
             context.Todos.Update(model);
             context.SaveChanges();
 
-            return model;
+            return Ok(model);
 
         }
 
         // Deletando o registro
         [HttpDelete("/{id:int}")]
-        public TodoModel Delete(
+        public IActionResult Delete(
                 [FromRoute] int id,
                 [FromServices] AppDbContext context)
         {
             var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return NotFound();
             context.Remove(model);
             context.SaveChanges();
-            return model;
+            return Ok(model);
         }
     }
 }
