5897cacad264a034d8f87c8d88f3fb86c02b9e7d
Warley Afonso <warleysilva365@gmail.com>
2023-05-28 16:41:52 -0300

    Subindo a configuração com o banco e a tabela do Todo

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo_API_ASPNET.Models;
+
+namespace Todo_API_ASPNET.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoModel> Todos{ get; set; }
+
+        // Configurando a conexão com o banco do sqlite
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
