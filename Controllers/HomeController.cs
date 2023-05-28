846b9a7eb61148ae68a1304c7b79cbf936d70208
Warley Afonso <warleysilva365@gmail.com>
2023-05-28 18:23:54 -0300

    Adicionando os métodos de buscar apenas 1, o update e o delete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,5 +23,46 @@ namespace Todo_API_ASPNET.Controllers
             context.SaveChanges();
             return todo;
         }
+        // Buscando por uma id
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById(
+                [FromRoute] int id,
+                [FromServices] AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id);
+        }
+
+        // Atualizando o registro
+        [HttpPut("/{id:int}")]
+        public TodoModel Put(
+                [FromRoute] int id,
+                [FromBody] TodoModel todo,
+                [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return todo;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+
+            return model;
+
+        }
+
+        // Deletando o registro
+        [HttpDelete("/{id:int}")]
+        public TodoModel Delete(
+                [FromRoute] int id,
+                [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            context.Remove(model);
+            context.SaveChanges();
+            return model;
+        }
     }
 }
